7520b4c1e7230e86ac03d57bd7585864bc54d290
AndresTeran <pipre94@gmail.com>
2017-04-05 13:16:11 -0500

    se crea httpMANAGER

--- app/src/main/java/com/example/pc_1/asynctask/HttpManager.java
+++ app/src/main/java/com/example/pc_1/asynctask/HttpManager.java
@@ -1,8 +1,39 @@
 package com.example.pc_1.asynctask;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by pc-16 on 5/04/17.
  */
 
 public class HttpManager {
+    public static String getData(String uri) throws IOException {
+        // Clase para manejar archivos
+        BufferedReader reader = null;
+        //Clase URL de java (manejar rutas)
+        //URL permite parsear las urls
+        URL url = new URL(uri);
+        //ESTA CLASE CONECTA A INTERNET
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        //Se declara objeto de tipo StrinBuilder, para manejar el tipo de archivo
+        StringBuilder stringBuilder= new StringBuilder();
+        //Permite conectar y leer de internet, new InputStreamReader(connection.getInputStream())), este archivo permite recibir un parametro
+        //y recibe archivo de la clase de internet
+        reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String line;
+        //verificar si el archivo llego o no
+        while((line = reader.readLine())!= null){
+            stringBuilder.append(line+"\n");
+
+        }
+        //tostring para hacer cadena
+        return  stringBuilder.toString();
+
+    }
 }
